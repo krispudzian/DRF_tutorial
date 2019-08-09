8fc292b414f57c2ecc359f0abd433c3cedecc552
Krzysztof Ograbek <k.ograbek@breuer-gmbh.de>
2019-08-09 12:45:42 +0200

    finish Hyperlinke part of tutorial

--- drf_tut/drf_tut/settings.py
+++ drf_tut/drf_tut/settings.py
@@ -53,6 +53,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 ROOT_URLCONF = 'drf_tut.urls'
 
 TEMPLATES = [
--- drf_tut/snippets/serializers.py
+++ drf_tut/snippets/serializers.py
@@ -3,19 +3,21 @@ from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
-
-
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Snippet
-        fields = ['id','title','code','linenos','language','style', 'owner']
-    
-    # Charfield(readOnly=True) equivalent
-    owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['url', 'id', 'username', 'snippets']
 
